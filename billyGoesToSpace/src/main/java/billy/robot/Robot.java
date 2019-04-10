ef369509d585e46d3c7258b5502ddc68e71c3e01
Billy Girard <21wgirard@wpsstudents.org>
2019-04-10 20:19:31 +0100

    Remove references to examples

--- billyGoesToSpace/src/main/java/billy/robot/Robot.java
+++ billyGoesToSpace/src/main/java/billy/robot/Robot.java
@@ -36,7 +36,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+    // m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
   }
